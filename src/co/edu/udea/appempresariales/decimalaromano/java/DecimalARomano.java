dc3d5adf42cba6ffa6b7b9576c5d1f029918f1b2
jodaheca <jdavidhc94@gmail.com>
2015-11-19 17:19:09 -0500

    Hago refactor del codigo

--- src/co/edu/udea/appempresariales/decimalaromano/java/DecimalARomano.java
+++ src/co/edu/udea/appempresariales/decimalaromano/java/DecimalARomano.java
@@ -2,21 +2,21 @@ package co.edu.udea.appempresariales.decimalaromano.java;
 
 public class DecimalARomano {
 	
-	String convertidos[] ;
-	
+	private String convertidos[] ;
+	private final String unidades[] = {"", "I", "II", "III", "IV", "V", "VI", "VII", "VIII", "IX"};
+	private int limite = 9;
 	public DecimalARomano(){
 		
 	}
 	
 	/**
-	 * 		
+	 * Metodo encargado de realizar la conversion de los numeros decimales a 
+	 * romano		
 	 * @return String[] Con los numeros convertidos a romano
 	 */
 	public String[] convertirDecimalARomano(){
 		
 		convertidos = new String[10];
-		String unidades[] = {"", "I", "II", "III", "IV", "V", "VI", "VII", "VIII", "IX"};
-		int limite = 9;
 		for (int i = 1; i <= limite; i++) {
 			convertidos[i] = unidades[i];
 		}
