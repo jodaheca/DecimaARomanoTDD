c2a9115151f03e249d5e39c18d63c9eb857c62fe
jodaheca <jdavidhc94@gmail.com>
2015-11-19 21:20:20 -0500

    Realizo la prueba unitaria que permite verificar que el metodo convierta los numero del 1 al 99 a romano

--- test/co/edu/udea/appempresariales/decimalaromano/java/DecimalARomanoTest.java
+++ test/co/edu/udea/appempresariales/decimalaromano/java/DecimalARomanoTest.java
@@ -10,6 +10,15 @@ public class DecimalARomanoTest {
 	DecimalARomano convertidor;
 	String convertido[];
 	private final String  ONETONINE ="--I-II-III-IV-V-VI-VII-VIII-IX";
+	private final String ONETONINETYNINE = "--I-II-III-IV-V-VI-VII-VIII-IX-X-XI-"
+			+ "XII-XIII-XIV-XV-XVI-XVII-XVIII-XIX-XX-XXI-XXII-XXIII-XXIV-XXV-"
+			+ "XXVI-XXVII-XXVIII-XXIX-XXX-XXXI-XXXII-XXXIII-XXXIV-XXXV-XXXVI-"
+			+ "XXXVII-XXXVIII-XXXIX-XL-XLI-XLII-XLIII-XLIV-XLV-XLVI-XLVII-"
+			+ "XLVIII-XLIX-L-LI-LII-LIII-LIV-LV-LVI-LVII-LVIII-LIX-LX-LXI-LXII-"
+			+ "LXIII-LXIV-LXV-LXVI-LXVII-LXVIII-LXIX-LXX-LXXI-LXXII-LXXIII-"
+			+ "LXXIV-LXXV-LXXVI-LXXVII-LXXVIII-LXXIX-LXXX-LXXXI-LXXXII-LXXXIII-"
+			+ "LXXXIV-LXXXV-LXXXVI-LXXXVII-LXXXVIII-LXXXIX-XC-XCI-XCII-XCIII-"
+			+ "XCIV-XCV-XCVI-XCVII-XCVIII-XCIX" ;
 	
 	/**
 	 * Prueba unitaria encargada de verificar que el metodo puedo convertir el numero 1 
@@ -41,8 +50,21 @@ public class DecimalARomanoTest {
 	public void converOneToNine() {
 		convertidor = new DecimalARomano();
 		convertido = convertidor.convertirDecimalARomano();
-		String oneToTenRomanos = String.join("-", convertido);
-		assertEquals(ONETONINE, oneToTenRomanos);
+		String oneToNineRomanos = String.join("-", convertido);
+		oneToNineRomanos = oneToNineRomanos.substring(0,30);
+		assertEquals(ONETONINE, oneToNineRomanos);
+	}
+	
+	/**
+	 * Prueba unitaria encargada de verificar que el metodo puedo convertir los numeros 
+	 * del 1 al 99 decimales a romanos
+	 */
+	@Test
+	public void converOneToNinetyNine() {
+		convertidor = new DecimalARomano();
+		convertido = convertidor.convertirDecimalARomano();
+		String oneToNinetyNineRomanos = String.join("-", convertido);
+		assertEquals(ONETONINETYNINE, oneToNinetyNineRomanos);
 	}
 
 }
